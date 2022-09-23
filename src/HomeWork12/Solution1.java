00f82da6be98a19961ca4771472c902073509cd9
Alexander Klubnikov <clubman066@gmail.com>
2022-09-23 15:08:29 +0300

    Adding solutions for HomeWork12

---
+++ src/HomeWork12/Solution1.java
@@ -0,0 +1,32 @@
+package HomeWork12;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter path to file...");
+        String pathToFile = scan.nextLine();
+        isValid(pathToFile);
+        scan.close();
+    }
+
+    public static void isValid(String pathToFile) {
+        try (Scanner fileScanner = new Scanner(new FileReader(pathToFile))) {
+
+            while (fileScanner.hasNext()) {
+                String docNum = fileScanner.nextLine();
+                if ((docNum.startsWith("docnum")
+                        || docNum.startsWith("contract"))
+                        && docNum.length() == 15) {
+                    System.out.println("Document number " + docNum + " is valid");
+                } else System.out.println("Document number " + docNum + " is invalid");
+            }
+
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
